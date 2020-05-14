93cd97e44546f119272cc0a0af4b6d01ec35d209
Heidi Ellison <hrod2645@gmail.com>
2020-05-14 01:52:06 -0600

    create search page

---
+++ lib/pages/timeline.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TimeLine extends StatefulWidget {
+  @override
+  _TimelineState createState() => _TimelineState();
+}
+
+class _TimelineState extends State<Timeline> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Timeline");
+  }
+}
