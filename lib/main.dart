7b4c68309be7f55a3e37f7437c759067e5e28962
Heidi Ellison <hrod2645@gmail.com>
2020-05-14 00:02:38 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'packege:flutter/materal.dart';
+import 'package:fluttershare/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'family_app',
+      debugShowCheckedModBanner: false,
+      home: Home(),
+    );
+  }
+}
